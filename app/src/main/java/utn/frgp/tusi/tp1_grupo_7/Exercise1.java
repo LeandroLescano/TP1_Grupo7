8ffb544d1cd7f3b32dcc58b8328c32dbb63853e5
Juan <49132815+juanccassano@users.noreply.github.com>
2020-08-19 22:08:05 -0300

    Funcionalidad y layout ejercicio 1

--- app/src/main/java/utn/frgp/tusi/tp1_grupo_7/Exercise1.java
+++ app/src/main/java/utn/frgp/tusi/tp1_grupo_7/Exercise1.java
@@ -1,5 +1,6 @@
 package utn.frgp.tusi.tp1_grupo_7;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -10,6 +11,10 @@ import utn.frgp.tusi.tp1_grupo_7.R;
 
 public class Exercise1 extends AppCompatActivity {
 
+    private EditText primerNum;
+    private EditText segNum;
+    private TextView res;
+
     @Override
     public void setTitle(int titleId) {
         super.setTitle("Suma");
@@ -19,6 +24,23 @@ public class Exercise1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.exercise1_layout);
+        primerNum = (EditText) findViewById(R.id.ednumUno);
+        segNum = (EditText) findViewById(R.id.ednumDos);
+        res = (TextView) findViewById(R.id.txtRes);
+    }
+
+    public void sumar(View view)
+    {
+    String valorUno = primerNum.getText().toString();
+    String valorDos = segNum.getText().toString();
+
+    int numUno = Integer.parseInt(valorUno);
+    int numDos = Integer.parseInt(valorDos);
+    int numRes = numUno + numDos;
+
+    String result = String.valueOf(numRes);
+    res.setText(result);
+
     }
 
 }
